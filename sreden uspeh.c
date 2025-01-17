3d84833c751d94c6802b2c65772004aaefcc436f
IvanPopov8a <ivanpopov20101208@gmail.com>
2025-01-17 16:03:43 +0200

    sredno

---
+++ sreden uspeh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int mark,i,n;
+    float avr,sum = 0;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for(i=1;i<=n;i++){
+        printf("enter a grade: ");
+        scanf("%d", &mark);
+
+        sum=sum + mark;
+    }
+
+    avr=sum / n;
+    printf("your grade is:  %.2f", avr);
+    return 0;
+        }
